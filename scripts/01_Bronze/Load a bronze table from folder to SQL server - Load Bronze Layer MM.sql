f9a910a9a79bbbc84ecd479ad97ab2e23cf818ba
Mariusz Makowski <mariuszmakowski73@gmail.com>
2025-11-15 20:54:52 +0000

    Add files via upload

---
+++ scripts/01_Bronze/Load a bronze table from folder to SQL server - Load Bronze Layer MM.sql
@@ -0,0 +1,377 @@
+﻿
+/*
+-- ============================================================================== --
+-- Stored Procedure: Load Bronze Layer (Source -> Bronze)                         --                
+-- ============================================================================== --
+-- Script Purpose:
+--  This stored procedure loads data into the 'bronze' schema from external CSV files. 
+--  It performs the following actions:
+--    - Truncates the bronze tables before loading data.
+--    - Uses the `BULK INSERT` command to load data from csv Files to bronze tables.
+-- ============================================================================== --
+-- Parameters:                                                                    --
+--   None.                                                                        --
+--	  This stored procedure does not accept any parameters or return any values.  --
+___________________________________
+***********************************
+Usage Example:
+    EXEC bronze.load_bronze;
+***********************************
+___________________________________
+-- ============================================================================== --
+*/
+
+CREATE OR ALTER PROCEDURE bronze.load_bronze AS
+BEGIN
+	DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME,
+		@batch_end_time DATETIME; 
+	BEGIN TRY
+		SET @batch_start_time = GETDATE();
+		PRINT '================================================';
+		PRINT 'Loading Bronze Layer';
+		PRINT '================================================';
+
+		PRINT '------------------------------------------------';
+		PRINT 'Loading CRM Tables';
+		PRINT '------------------------------------------------';
+
+-- *************************** --
+-- bronze.crm_cust_info        --
+-- *************************** --
+-- ============================================================================== --
+-- Start a per-step timer                                                         --
+-- ============================================================================== --
+		SET @start_time = GETDATE();
+		
+-- ============================================================================== --
+-- Log what's about to happen                                                     --
+-- ============================================================================== --
+		PRINT '>> Truncating Table: bronze.crm_cust_info'; 
+
+-- ============================================================================== --
+-- Fast, minimally-logged way to empty the table (also resets identity)           --
+-- ============================================================================== --
+		TRUNCATE TABLE bronze.crm_cust_info;
+
+-- ============================================================================== --
+-- Log next step                                                                  --
+-- ============================================================================== --
+		PRINT '>> Inserting Data Into: bronze.crm_cust_info';
+
+-- ============================================================================== --
+-- Load CSV → table                                                               --
+-- ============================================================================== --
+		BULK INSERT bronze.crm_cust_info
+		FROM 'C:\Users\mmako\Dropbox\# IBM data since training\MySQL\DATA PROJECT 
+		01\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+		WITH 
+		(
+			FIRSTROW = 2,         -- skip header row (data starts on line 2)
+			FIELDTERMINATOR = ',',-- columns are comma-separated
+			TABLOCK               -- take a table lock for faster bulk load
+		);
+		
+-- ============================================================================== --
+-- Stop the timer                                                                 --
+-- ============================================================================== --
+		SET @end_time = GETDATE();
+		
+-- ============================================================================== --
+-- Print the duration of this step in seconds                                     --
+-- ============================================================================== --
+		PRINT '>> Load Duration: 
+		' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '>> -------------';
+
+
+
+-- *************************** --
+-- bronze.crm_prd_info         --
+-- *************************** --
+-- ============================================================================== --
+-- Start a per-step timer                                                         --
+-- ============================================================================== --
+        SET @start_time = GETDATE();
+
+-- ============================================================================== --
+-- Log what's about to happen
+-- ============================================================================== --
+		PRINT '>> Truncating Table: bronze.crm_prd_info';
+
+-- ============================================================================== --
+-- Fast, minimally-logged way to empty the table (also resets identity)           --
+-- ============================================================================== --
+		TRUNCATE TABLE bronze.crm_prd_info;
+
+-- ============================================================================== --
+-- Log next step                                                                  --
+-- ============================================================================== --
+		PRINT '>> Inserting Data Into: bronze.crm_prd_info';
+
+-- ============================================================================== --
+-- Load CSV → table                                                               --
+-- ============================================================================== --
+		BULK INSERT bronze.crm_prd_info
+		FROM 'C:\Users\mmako\Dropbox\# IBM data since training\MySQL\DATA PROJECT 
+		01\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+		WITH 
+		(
+			FIRSTROW = 2,         -- skip header row (data starts on line 2)
+			FIELDTERMINATOR = ',',-- columns are comma-separated
+			TABLOCK               -- take a table lock for faster bulk load
+		);
+
+-- ============================================================================== --
+-- Stop the timer                                                                 --
+-- ============================================================================== --	
+		SET @end_time = GETDATE();
+		
+-- ============================================================================== --
+-- Print the duration of this step in seconds                                     --
+-- ============================================================================== --
+		
+		PRINT '>> Load Duration: 
+		' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '>> -------------';
+
+
+
+
+
+
+
+
+-- *************************** --
+-- bronze.crm_sales_details    --
+-- *************************** --
+-- ============================================================================== --
+-- Start a per-step timer                                                         --
+-- ============================================================================== --
+        SET @start_time = GETDATE();
+
+-- ============================================================================== --
+-- Log what's about to happen                                                     --
+-- ============================================================================== --
+		PRINT '>> Truncating Table: bronze.crm_sales_details';
+
+-- ============================================================================== --
+-- Fast, minimally-logged way to empty the table (also resets identity)           --
+-- ============================================================================== --
+		TRUNCATE TABLE bronze.crm_sales_details;
+
+-- ============================================================================== --
+-- Log next step                                                                  --
+-- ============================================================================== --
+		PRINT '>> Inserting Data Into: bronze.crm_sales_details';
+
+-- ============================================================================== --
+-- Load CSV → table                                                               --
+-- ============================================================================== --	
+		BULK INSERT bronze.crm_sales_details
+		FROM 'C:\Users\mmako\Dropbox\# IBM data since training\MySQL\DATA PROJECT 
+		01\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+		WITH 
+		(
+			FIRSTROW = 2,         -- skip header row (data starts on line 2)
+			FIELDTERMINATOR = ',',-- columns are comma-separated
+			TABLOCK               -- take a table lock for faster bulk load
+		);
+		
+-- ============================================================================== --
+-- Stop the timer                                                                 --
+-- ============================================================================== --
+		SET @end_time = GETDATE();
+		
+-- ============================================================================== --
+-- Print the duration of this step in seconds                                     --
+-- ============================================================================== --
+		PRINT '>> Load Duration: 
+		' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '>> -------------';
+
+		PRINT '------------------------------------------------';
+		PRINT 'Loading ERP Tables';
+		PRINT '------------------------------------------------';
+		
+
+
+
+
+
+
+-- *************************** --
+-- bronze.erp_loc_a101         --
+-- *************************** --
+-- ============================================================================== --
+-- Start a per-step timer                                                         --
+-- ============================================================================== --
+		SET @start_time = GETDATE();
+
+-- ============================================================================== --
+-- Log what's about to happen                                                     --
+-- ============================================================================== --
+		PRINT '>> Truncating Table: bronze.erp_loc_a101';
+
+-- ============================================================================== --
+-- Fast, minimally-logged way to empty the table (also resets identity)           --
+-- ============================================================================== --
+		TRUNCATE TABLE bronze.erp_loc_a101;
+
+-- ============================================================================== --
+-- Log next step                                                                  --
+-- ============================================================================== --
+		PRINT '>> Inserting Data Into: bronze.erp_loc_a101';
+
+-- ============================================================================== --
+-- Load CSV → table                                                               --
+-- ============================================================================== --
+		BULK INSERT bronze.erp_loc_a101
+		FROM 'C:\Users\mmako\Dropbox\# IBM data since training\MySQL\DATA PROJECT 
+		01\sql-data-warehouse-project\datasets\source_erp\loc_a101.csv'
+		WITH 
+		(
+			FIRSTROW = 2,         -- skip header row (data starts on line 2)
+			FIELDTERMINATOR = ',',-- columns are comma-separated
+			TABLOCK               -- take a table lock for faster bulk load
+		);
+		
+-- ============================================================================== --
+-- Stop the timer                                                                 --
+-- ============================================================================== --
+		SET @end_time = GETDATE();
+		
+-- ============================================================================== --
+-- Print the duration of this step in seconds                                     --
+-- ============================================================================== --
+		PRINT '>> Load Duration: 
+		' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '>> -------------';
+
+
+
+-- *************************** --
+-- bronze.erp_cust_az12        --
+-- *************************** --
+-- ============================================================================== --
+-- Start a per-step timer                                                         --
+-- ============================================================================== --
+		SET @start_time = GETDATE();
+
+-- ============================================================================== --
+-- Log what's about to happen                                                     --
+-- ============================================================================== --
+		PRINT '>> Truncating Table: bronze.erp_cust_az12';
+
+-- ============================================================================== --
+-- Fast, minimally-logged way to empty the table (also resets identity)           --
+-- ============================================================================== --
+		TRUNCATE TABLE bronze.erp_cust_az12;
+
+-- ============================================================================== --
+-- Log next step                                                                  --
+-- ============================================================================== --
+		PRINT '>> Inserting Data Into: bronze.erp_cust_az12';
+
+-- ============================================================================== --
+-- Load CSV → table                                                               --
+-- ============================================================================== --
+		BULK INSERT bronze.erp_cust_az12
+		FROM 'C:\Users\mmako\Dropbox\# IBM data since training\MySQL\DATA PROJECT 
+		01\sql-data-warehouse-project\datasets\source_erp\cust_az12.csv'
+		WITH 
+		(
+			FIRSTROW = 2,         -- skip header row (data starts on line 2)
+			FIELDTERMINATOR = ',',-- columns are comma-separated
+			TABLOCK               -- take a table lock for faster bulk load
+		);
+		
+-- ============================================================================== --
+-- Stop the timer                                                                 --
+-- ============================================================================== --
+		SET @end_time = GETDATE();
+		
+-- ============================================================================== --
+-- Print the duration of this step in seconds                                     --
+-- ============================================================================== --
+		PRINT '>> Load Duration: 
+		' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '>> -------------';
+
+
+
+-- *************************** --
+-- bronze.erp_px_cat_g1v2      --
+-- *************************** --
+-- ============================================================================== --
+-- Start a per-step timer                                                         --
+-- ============================================================================== --
+		SET @start_time = GETDATE();
+
+-- ============================================================================== --
+-- Log what's about to happen                                                     --
+-- ============================================================================== --
+		PRINT '>> Truncating Table: bronze.erp_px_cat_g1v2';
+
+-- ============================================================================== --
+-- Fast, minimally-logged way to empty the table (also resets identity)           --
+-- ============================================================================== --
+		TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+
+-- ============================================================================== --
+-- Log next step                                                                  --
+-- ============================================================================== --
+		PRINT '>> Inserting Data Into: bronze.erp_px_cat_g1v2';
+
+-- ============================================================================== --
+-- Load CSV → table                                                               --
+-- ============================================================================== --
+		BULK INSERT bronze.erp_px_cat_g1v2
+		FROM 'C:\Users\mmako\Dropbox\# IBM data since training\MySQL\DATA PROJECT 
+		01\sql-data-warehouse-project\datasets\source_erp\px_cat_g1v2.csv'
+		WITH 
+		(
+			FIRSTROW = 2,         -- skip header row (data starts on line 2)
+			FIELDTERMINATOR = ',',-- columns are comma-separated
+			TABLOCK               -- take a table lock for faster bulk load
+		);
+		
+-- ============================================================================== --
+-- Stop the timer                                                                 --
+-- ============================================================================== --
+		SET @end_time = GETDATE();
+		
+-- ============================================================================== --
+-- Print the duration of this step in seconds                                     --
+-- ============================================================================== --
+		PRINT '>> Load Duration: 
+		' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '>> -------------'; 
+
+
+
+-- ============================================================================== --
+-- Stop the timer                                                                 --
+-- ============================================================================== --
+		SET @batch_end_time = GETDATE();
+
+-- ============================================================================== --
+-- Log what's about to happen                                                     --
+-- ============================================================================== --
+		PRINT '=========================================='
+		PRINT 'Loading Bronze Layer is Completed';
+        PRINT '   - Total Load Duration: 
+		' + CAST(DATEDIFF(SECOND, @batch_start_time, @batch_end_time) AS NVARCHAR) + 
+		' seconds';
+		PRINT '=========================================='
+	END TRY
+	BEGIN CATCH
+		PRINT '=========================================='
+		PRINT 'ERROR OCCURED DURING LOADING BRONZE LAYER'
+		PRINT 'Error Message' + ERROR_MESSAGE();
+		PRINT 'Error Message' + CAST (ERROR_NUMBER() AS NVARCHAR);
+		PRINT 'Error Message' + CAST (ERROR_STATE() AS NVARCHAR);
+		PRINT '=========================================='
+	END CATCH
+END
+
+
